8f2f6d3be3e21d2971104c067ef7c1e28b9ae4c8
yamileerbes <129629251+yamileerbes@users.noreply.github.com>
2025-05-12 17:58:00 -0300

    web

---
+++ TDMM2/script.js
@@ -0,0 +1,4 @@
+function toggleForm() {
+  const form = document.getElementById("formContainer");
+  form.style.display = (form.style.display === "none" || form.style.display === "") ? "block" : "none";
+}
